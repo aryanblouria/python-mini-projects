eb8ebf2ee64f57ae859ce67e1c796612eb983f4c
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2021-09-19 20:29:49 +0530

    Update leetify.py

--- Leetify/leetify.py
+++ Leetify/leetify.py
@@ -1,38 +1,10 @@
 import random
 import pyperclip
+from leetmap import leetmap
 
 
 def leetify(message):
     leet = ""
-    leetmap = {
-        'a': ['4', '@'],
-        'b': ['13', '6'],
-        'c': ['(', '{'],
-        'd': ['[)', '|)'],
-        'e': ['3', '[-'],
-        'f': ['|=', ']='],
-        'g': ['9', '(_+'],
-        'h': ['#', '|-|'],
-        'i': ['!', '|'],
-        'j': ['_|', '_)'],
-        'k': ['|<', '|{'],
-        'l': ['1', '|_'],
-        'm': ['|\/|', '44'],
-        'n': ['|\|', '/\/'],
-        'o': ['0', '()'],
-        'p': ['|>', '|o'],
-        'q': ['(,)', '0,'],
-        'r': ['|2', '/2'],
-        's': ['$', '5'],
-        't': ['-|-', '+'],
-        'u': ['(_)', '[_]'],
-        'v': ['\/'],
-        'w': ['\/\/', 'vv'],
-        'x': ['><', '}{'],
-        'y': ['`/', '`('],
-        'z': ['2', '3']
-    }
-
     for char in message:
         if char.lower() in leetmap.keys() and random.random() < 0.5:
             leet += random.choice(leetmap[char.lower()])
@@ -41,7 +13,8 @@ def leetify(message):
     return leet
 
 
-while True:
+stop = False
+while not stop:
     original = input("\nEnter the message: ")
     leeted = leetify(original)
     pyperclip.copy(leeted)
@@ -51,3 +24,4 @@ while True:
     while to_continue.lower() == 'n':
         print("\nWhy would you not want to leetify all day every day? Please reconsider.")
         to_continue = input("Would you like to leetify another message? (y/n): ")
+
